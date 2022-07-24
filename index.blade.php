b1807272f19e2afc367975a9f8aee5b8728bc6cd
محسن مجدم <56688766+mohsenmojadam2019@users.noreply.github.com>
2022-07-24 15:02:49 +0430

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+<body>
+
+<h2>HTML Table</h2>
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Family</th>
+        <th>Date of birth</th>
+    </tr>
+    <tr>
+        <td>Carol</td>
+        <td>Kline</td>
+        <td>10-07-94</td>
+    </tr>
+    <tr>
+        <td>Pearl</td>
+        <td>Kane</td>
+        <td>06-03-83</td>
+    </tr>
+    <tr>
+        <td>Tyrell</td>
+        <td>Taun</td>
+        <td>11-09-01</td>
+    </tr>
+</table>
+
+</body>
+</html>
+
